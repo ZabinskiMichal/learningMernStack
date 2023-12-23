070a0ea1595926a135b737cddf86fe4d044b7ae1
Michal Zabinski <zabinski@student.agh.edu.pl>
2023-12-23 16:03:58 +0100

    changed

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useSignup } from '../hooks/useSignup'
 
 
-export default function Signup() {
+export default async function Signup() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const{ signup, error, isLoading } = useSignup()
