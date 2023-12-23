7062ab6ebb78352d1f22b9bbb3f736a8040eed1a
Michal Zabinski <zabinski@student.agh.edu.pl>
2023-12-23 15:25:09 +0100

    added build folder1

---
+++ frontend/build/precache-manifest.5382491577ae260b0bd48b0b43b14f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8178b5fc68c32d31df2f581d78dd2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c6664a3f64e5e35770a",
+    "url": "/static/css/main.10edf31c.chunk.css"
+  },
+  {
+    "revision": "02f29407d6e7a4ccdd49",
+    "url": "/static/js/2.cd075009.chunk.js"
+  },
+  {
+    "revision": "288f8795c77f6dd607dd5879eebc8c76",
+    "url": "/static/js/2.cd075009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6664a3f64e5e35770a",
+    "url": "/static/js/main.abcfa5e2.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
